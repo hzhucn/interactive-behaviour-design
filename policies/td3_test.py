1f258c3c743d254ffd1aa6b48296f92a9bb15c29
Matthew Rahtz <matthew.rahtz@gmail.com>
2019-03-28 15:48:54 -0700

    Add zigzag test

--- policies/td3_test.py
+++ policies/td3_test.py
@@ -23,56 +23,81 @@ tf.logging.set_verbosity(tf.logging.ERROR)
 register()
 
 
-def get_action_smooth(obs):
-    assert obs.shape == (7,)
-    gripper_to_block = obs[:3]
-    block_to_target = obs[3:6]
-    gripper_width = obs[6]
-    if (np.linalg.norm(gripper_to_block) > 0.01 and
-            np.linalg.norm(block_to_target) > 0.1):  # Don't open grippers if blocking is slipping near target
-        if np.linalg.norm(gripper_to_block[:2]) > 0.05:
-            z = 0
+class Oracle:
+    def __init__(self, mode):
+        self.gripping = None
+        self.step_n = None
+        self.last_action = None
+        self.mode = mode
+
+    def reset(self):
+        self.gripping = False
+        self.step_n = 0
+
+    def get_action(self, obs):
+        if self.mode == 'smooth':
+            return self.get_action_smooth(obs)
+        elif self.mode == 'zigzag':
+            return self.get_action_zigzag(obs)
         else:
-            z = gripper_to_block[2]
-        action = np.concatenate([3 * gripper_to_block[:2], [3 * z, 1]])
-    elif gripper_width > 0.05:
-        action = np.array([0, 0, 0, -1])
-    else:
-        action = np.concatenate([3 * block_to_target, [-1]])
-    return action
+            raise Exception()
+
+    def get_action_smooth(self, obs):
+        gripper_to_block = obs[:3]
+        block_to_target = obs[3:6]
+        gripper_width = obs[6]
+        if np.linalg.norm(gripper_to_block) > 0.01 and not self.gripping:
+            if np.linalg.norm(gripper_to_block[:2]) > 0.05:
+                z = 0
+            else:
+                z = gripper_to_block[2]
+            action = np.concatenate([3 * gripper_to_block[:2], [3 * z, 1]])
+        elif gripper_width > 0.05:
+            action = np.array([0, 0, 0, -1])
+        else:
+            self.gripping = True
+            action = np.concatenate([3 * block_to_target, [-1]])
+        return action
+
+    def get_action_zigzag(self, obs):
+        if self.step_n % 5 != 0:
+            self.step_n += 1
+            return self.last_action
+
+        gripper_to_block = obs[:3]
+        block_to_target = obs[3:6]
+        gripper_width = obs[6]
+        if np.linalg.norm(gripper_to_block) > 0.02 and not self.gripping:
+            di = np.argmax(np.abs(gripper_to_block))
+            action = np.array([0., 0., 0., 1.])
+            action[di] = 0.3 * np.sign(gripper_to_block[di])
+        elif gripper_width > 0.055:
+            action = [0., 0., 0., -0.02]
+        else:
+            self.gripping = True
+            di = np.argmax(np.abs(block_to_target))
+            action = np.array([0., 0., 0., -1.])
+            action[di] = 0.3 * np.sign(block_to_target[di])
+
+        self.last_action = action
+        self.step_n += 1
+        return action
 
 
-def get_action_jerky(obs):
-    assert obs.shape == (7,)
-    gripper_to_block = obs[:3]
-    block_to_target = obs[3:6]
-    gripper_width = obs[6]
-    if (np.linalg.norm(gripper_to_block) > 0.03 and
-            np.linalg.norm(block_to_target) > 0.1):  # Don't open grippers if blocking is slipping near target
-        di = np.argmax(np.abs(gripper_to_block))
-        action = np.array([0., 0., 0., 1.])
-        action[di] = 0.15 * np.sign(gripper_to_block[di])
-    elif gripper_width > 0.05:
-        action = np.array([0, 0, 0, -1])
-    else:
-        di = np.argmax(np.abs(block_to_target))
-        action = np.array([0., 0., 0., -1.])
-        action[di] = 0.15 * np.sign(block_to_target[di])
-    return action
-
-
-def gen_demonstrations(env_id, log_dir, n_demonstrations, demonstrations_buffer: DemonstrationsBuffer):
+def gen_demonstrations(env_id, log_dir, n_demonstrations, demonstrations_buffer: DemonstrationsBuffer, oracle):
     env = gym.make(env_id)
     env.seed(0)
     np.random.seed(0)
+    # env = Monitor(env, video_callable=lambda n: True, directory=log_dir, uid=111)
     env = RandomInitialPosition(env)
     env = LogEpisodeStats(env, log_dir, '_demo')
 
     for n in range(n_demonstrations):
         print(f"Generating demonstration {n}...")
         obs, done = env.reset(), False
+        oracle.reset()
         while not done:
-            action = get_action_smooth(obs)
+            action = oracle.get_action(obs)
             demonstrations_buffer.store(obs, action)
             obs, reward, done, info = env.step(action)
 
@@ -115,22 +140,22 @@ class TestTD3(unittest.TestCase):
     fetch_hyperparams = dict()
 
     def test_cheetah_single(self):
-        test_ret = self.run_td3('HalfCheetah-v2', n_envs=1, n_epochs=10, hyperparams=self.spinningup_hyperparams)
+        test_ret = self.run_td3_rl('HalfCheetah-v2', n_envs=1, n_epochs=10, hyperparams=self.spinningup_hyperparams)
         self.assertGreater(test_ret, 2000)
 
     def test_cheetah_parallel(self):
-        test_ret = self.run_td3('HalfCheetah-v2', n_envs=10, n_epochs=5, hyperparams=self.spinningup_hyperparams)
+        test_ret = self.run_td3_rl('HalfCheetah-v2', n_envs=10, n_epochs=5, hyperparams=self.spinningup_hyperparams)
         self.assertGreater(test_ret, 1500)
 
     def test_reach_single(self):
-        test_ret = self.run_td3('FetchReachDense-v1', n_envs=1, n_epochs=5, hyperparams=self.fetch_hyperparams)
+        test_ret = self.run_td3_rl('FetchReachDense-v1', n_envs=1, n_epochs=5, hyperparams=self.fetch_hyperparams)
         self.assertGreater(test_ret, -1.0)
 
     def test_reach_parallel(self):
-        test_ret = self.run_td3('FetchReachDense-v1', n_envs=16, n_epochs=2, hyperparams=self.fetch_hyperparams)
+        test_ret = self.run_td3_rl('FetchReachDense-v1', n_envs=16, n_epochs=2, hyperparams=self.fetch_hyperparams)
         self.assertGreater(test_ret, -1.0)
 
-    def run_td3(self, env_id, n_envs, n_epochs, hyperparams):
+    def run_td3_rl(self, env_id, n_envs, n_epochs, hyperparams):
         tmp_dir = tempfile.mkdtemp()
         print("Logging to", tmp_dir)
         train_env = CustomSubprocVecEnv(env_fns=[lambda env_n=env_n: self.env_fn(env_id=env_id, seed=env_n)
@@ -172,7 +197,15 @@ class TestTD3(unittest.TestCase):
 
         return test_return
 
-    def test_pp_bc(self):
+    def test_pickplace_smooth_demonstrations(self):
+        oracle = Oracle('smooth')
+        self.run_td3_bc(oracle)
+
+    def test_pickplace_zigzag_demonstrations(self):
+        oracle = Oracle('zigzag')
+        self.run_td3_bc(oracle)
+
+    def run_td3_bc(self, oracle):
         env_id = 'FetchPickAndPlace-Repeat1-ContGripper-WithGripObs-5InitialBlockPos-FixedGoal-Delta-GripperBonuses-v0'
         temp_dir = tempfile.mkdtemp()
         print("Logging to", temp_dir)
@@ -193,12 +226,12 @@ class TestTD3(unittest.TestCase):
 
         test_env = gym.make(env_id)
         test_env.seed(0)
-        # test_env = Monitor(test_env, directory=temp_dir, video_callable=lambda n: True)
+        test_env = Monitor(test_env, directory=temp_dir, video_callable=lambda n: True)
         test_env = LogEpisodeStats(test_env, log_dir=temp_dir, stdout=False)
         policy.test_env = test_env
 
         policy.init_logger(temp_dir)
-        gen_demonstrations(env_id, os.path.join(temp_dir, 'demos'), n_demos, policy.demonstrations_buffer)
+        gen_demonstrations(env_id, os.path.join(temp_dir, 'demos'), n_demos, policy.demonstrations_buffer, oracle)
         last_epoch_n = None
         while policy.epoch_n < 10:
             if policy.epoch_n != last_epoch_n:
